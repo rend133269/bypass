0a1f07849acb32a2c59d7c5d838d1493ccbb1c8e
rend133269 <165554050+rend133269@users.noreply.github.com>
2024-05-31 15:05:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,35 @@
+<?php
+
+
+
+
+
+echo geturl($_GET['url']);
+
+    function geturl($url) { 
+
+        $headers[] = 'Accept: image/gif, image/x-bitmap, image/jpeg, image/pjpeg,text/html,application/xhtml+xml'; 
+        $headers[] = 'Connection: Keep-Alive'; 
+        $headers[] = 'Content-type: application/x-www-form-urlencoded;charset=UTF-8'; 
+        $useragent = 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 1.0.3705; .NET CLR 1.1.4322; Media Center PC 4.0)'; 
+        //andithomas
+        $process = curl_init($url); 
+        curl_setopt($process, CURLOPT_HTTPHEADER, $headers); 
+        curl_setopt($process, CURLOPT_HEADER, 0); 
+        curl_setopt($process, CURLOPT_USERAGENT, $useragent);
+        curl_setopt($process, CURLOPT_TIMEOUT, 30); 
+        curl_setopt($process, CURLOPT_RETURNTRANSFER, 1); 
+        curl_setopt($process, CURLOPT_FOLLOWLOCATION, 1); 
+       // curl_setopt($process, CURLOPT_PROXY, "140.83.37.54");
+        //curl_setopt($process, CURLOPT_PROXYPORT, "80");
+
+
+        //andithomas
+        $return = curl_exec($process); 
+        curl_close($process); 
+        
+        return $return; 
+    } 
+    
+?>
 
